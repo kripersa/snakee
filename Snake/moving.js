0422553e21d42642bd39152eae65cdd507a9d830
kripersa <100306879+kripersa@users.noreply.github.com>
2023-05-29 02:28:59 +0400

    moveeeee

--- Snake/moving.js
+++ Snake/moving.js
@@ -7,37 +7,104 @@ export function moving() {
 
   function snakeMove(e) {
     let snakeSartinPosition = snake.parentElement.id;
-    console.log(snakeSartinPosition);
+
     let key = e.key;
     up(key, snakeSartinPosition);
     down(key, snakeSartinPosition);
     right(key, snakeSartinPosition);
     left(key, snakeSartinPosition);
+    console.log();
   }
 
   function up(key, snakeSartinPosition) {
-    if (key === "w")
-      if (snakeSartinPosition.length === 2) {
-        console.log(snakeSartinPosition.length);
-        let snakeCurentPosition =
-          (Number(snakeSartinPosition[0]) - 1).toString() +
-          Number(snakeSartinPosition[1]).toString();
+    if (key === "w" && snakeSartinPosition.length === 2);
+    let snakeCurentPosition =
+      (Number(snakeSartinPosition[0]) - 1).toString() +
+      Number(snakeSartinPosition[1]).toString();
+    console.log(snakeSartinPosition, snakeCurentPosition);
 
-        console.log(snakeCurentPosition);
-        for (let i = 0; i < cellArray.length; i++) {
-          if (cellArray[i].id === snakeCurentPosition) {
-            snake.parentElement.innerHTML = "";
-            cellArray[i].appendChild(snake);
-          }
+    for (let i = 0; i < cellArray.length; i++) {
+      if (cellArray[i].id === snakeCurentPosition) {
+        snake.parentElement.innerHTML = "";
+        cellArray[i].appendChild(snake);
+      }
+    }
+
+    if (
+      key === "w" &&
+      snakeSartinPosition.length === 3 &&
+      snakeSartinPosition[2] !== 0
+    ) {
+      let snakeCurentPosition =
+        (
+          Number(snakeSartinPosition[0] + snakeSartinPosition[1]) - 1
+        ).toString() + snakeSartinPosition[2];
+      console.log(snakeSartinPosition, snakeCurentPosition);
+
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
         }
       }
+    }
+    if (
+      key === "w" &&
+      snakeSartinPosition.length === 3 &&
+      snakeSartinPosition[2] == 0
+    ) {
+      let snakeCurentPosition =
+        (Number(snakeSartinPosition[0]) - 1).toString() +
+        snakeSartinPosition[1] +
+        snakeSartinPosition[2];
+      console.log("sdfffffffff");
+
+      console.log(snakeSartinPosition, snakeCurentPosition);
+
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
+    if (key === "w" && snakeSartinPosition.length === 4) {
+      let snakeCurentPosition =
+        (
+          Number(snakeSartinPosition[0] + snakeSartinPosition[1]) - 1
+        ).toString() +
+        Number(snakeSartinPosition[2]).toString() +
+        Number(snakeSartinPosition[3]).toString();
+      console.log(snakeSartinPosition, snakeCurentPosition);
+
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
   }
   function down(key, snakeSartinPosition) {
     if (key === "s") {
       let snakeCurentPosition =
         (Number(snakeSartinPosition[0]) + 1).toString() +
         Number(snakeSartinPosition[1]).toString();
-      console.log(snakeCurentPosition);
+      console.log(snakeSartinPosition, snakeCurentPosition);
+
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
+    if (key === "s" && snakeSartinPosition.length === 3) {
+      console.log("dabla");
+      let snakeCurentPosition =
+        (Number(snakeSartinPosition[0]) + 1).toString() +
+        snakeSartinPosition[1] +
+        snakeSartinPosition[2];
       for (let i = 0; i < cellArray.length; i++) {
         if (cellArray[i].id === snakeCurentPosition) {
           snake.parentElement.innerHTML = "";
@@ -51,19 +118,7 @@ export function moving() {
       let snakeCurentPosition =
         Number(snakeSartinPosition[0]).toString() +
         (Number(snakeSartinPosition[1]) - 1).toString();
-      console.log(snakeCurentPosition);
-
-      for (let i = 0; i < cellArray.length; i++) {
-        if (cellArray[i].id === snakeCurentPosition) {
-          snake.parentElement.innerHTML = "";
-          cellArray[i].appendChild(snake);
-        }
-      }}
-      if (key === "a" && snakeSartinPosition.length === 3) {
-        let snakeCurentPosition =
-        Number(snakeSartinPosition[0]).toString() +
-        (Number(snakeSartinPosition[1] + snakeSartinPosition[2]) - 1).toString();
-        console.log(snakeSartinPosition);
+      console.log(snakeSartinPosition, snakeCurentPosition);
 
       for (let i = 0; i < cellArray.length; i++) {
         if (cellArray[i].id === snakeCurentPosition) {
@@ -71,28 +126,101 @@ export function moving() {
           cellArray[i].appendChild(snake);
         }
       }
+    }
+    if (key === "a" && snakeSartinPosition.length === 3) {
+      let snakeCurentPosition =
+        Number(snakeSartinPosition[0]).toString() +
+        (
+          Number(snakeSartinPosition[1] + snakeSartinPosition[2]) - 1
+        ).toString();
+      console.log(snakeSartinPosition, snakeCurentPosition);
 
-        console.log("hjhj");
-      
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
+    if (
+      key === "a" &&
+      snakeSartinPosition.length === 3 &&
+      snakeSartinPosition[1] == 0
+    ) {
+      console.log("bklsklklsk");
+      let snakeCurentPosition =
+        snakeSartinPosition[0] +
+        snakeSartinPosition[1] +
+        (Number(snakeSartinPosition[2]) - 1).toString();
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
+    if (key === "a" && snakeSartinPosition.length === 4) {
+      let snakeCurentPosition =
+        snakeSartinPosition[0] +
+        snakeSartinPosition[1] +
+        (
+          Number(snakeSartinPosition[2] + snakeSartinPosition[3]) - 1
+        ).toString();
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
     }
   }
 
   function right(key, snakeSartinPosition) {
-    if (key === "d") {
+    if (key === "d" && snakeSartinPosition.length === 2) {
       let snakeCurentPosition =
         Number(snakeSartinPosition[0]).toString() +
         (Number(snakeSartinPosition[1]) + 1).toString();
-      console.log(snakeCurentPosition);
-      console.log(snakeCurentPosition.length);
-      console.log(snakeCurentPosition[0]);
-      console.log(snakeCurentPosition[1]);
-      console.log(snakeCurentPosition[2]);
-      if (snakeSartinPosition.length == 2) {
-        for (let i = 0; i < cellArray.length; i++) {
-          if (cellArray[i].id === snakeCurentPosition) {
-            snake.parentElement.innerHTML = "";
-            cellArray[i].appendChild(snake);
-          }
+      console.log(snakeSartinPosition, snakeCurentPosition);
+
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
+    if (
+      key === "d" &&
+      snakeSartinPosition.length === 3 &&
+      snakeSartinPosition[1] !== 0
+    ) {
+      let snakeCurentPosition =
+        Number(snakeSartinPosition[0]).toString() +
+        Number(snakeSartinPosition[1]).toString() +
+        Number(snakeSartinPosition[2]).toString();
+      console.log(snakeSartinPosition, snakeCurentPosition);
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
+        }
+      }
+    }
+    if (
+      key === "d" &&
+      snakeSartinPosition.length === 3 &&
+      snakeSartinPosition[1] == 0
+    ) {
+      let snakeCurentPosition =
+        Number(snakeSartinPosition[0]).toString() +
+        Number(snakeSartinPosition[1]).toString() +
+        (Number(snakeSartinPosition[2]) + 1).toString();
+      console.log(snakeSartinPosition, snakeCurentPosition);
+
+      for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].id === snakeCurentPosition) {
+          snake.parentElement.innerHTML = "";
+          cellArray[i].appendChild(snake);
         }
       }
     }
